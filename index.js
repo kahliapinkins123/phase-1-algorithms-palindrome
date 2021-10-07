1759a800d25fadb1a5a6527eef0b96fe708e2620
Kahlia Pinkins <kahliapinkins123@gmail.com>
2021-10-06 21:44:37 -0400

    commit tree e1692ee8a8e5a81adae53cff7b30ffc1a26e892f

--- index.js
+++ index.js
@@ -1,14 +1,50 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  //checks if word backwards matches word forward
+const wordBackward = makesBackward(word);
+return wordBackward === word;
+ }
+ 
+//Makes a backwards word
+function makesBackward(word){
+ let wordArray = word.split('')
+ let array = [];
+   for(let i= wordArray.length-1; i>=0; i--){
+     array.push(wordArray[i]);
+   }
+   return array.join('');
+ //return word.split('').reverse().join('') (shorter solution)
 }
+
+
 
 /* 
   Add your pseudocode here
+  create function that deals w palindrome{
+	
+      takes word and makes it backwards by running through each letter in 
+      the word starting from the last letter and concatenating it into a 
+      new string (function)
+
+        if backwardsWord == word{
+        return true
+        }
+
+        else{
+          return false
+        }
+
 */
 
 /*
   Add written explanation of your solution here
+
+  First I made the word backwords. Then, I compared the backwards word to
+  the original to see if they were the same. If they were, I returned true,
+  if not, false.
 */
+
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
@@ -20,6 +56,9 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log('Expecting: true');
+  console.log('=>', isPalindrome('mom'));
 }
 
 module.exports = isPalindrome;
